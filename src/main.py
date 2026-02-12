6de6479ee1899a0ea9d6c5173b080adfe3f6558a
htethlaingwin <htethlaingwin.bsn29@gmail.com>
2026-02-12 23:17:29 +0630

    deploy

--- src/main.py
+++ src/main.py
@@ -65,7 +65,7 @@ def main():
     if len(sys.argv) > 1:
         base_path = sys.argv[1]
     copy_static_files("static", "docs")
-    generate_page_recursive("content", "template.html", "public", base_path)
+    generate_page_recursive("content", "template.html", "docs", base_path)
 
 
 main()
